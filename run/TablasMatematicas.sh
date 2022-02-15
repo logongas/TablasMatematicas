1e2bf01ef44e46ef0e388ae0acdfadd4eb687005
Lorenzo <lorenzo.profesor@gmail.com>
2022-02-15 20:25:49 +0100

    No cache

--- run/TablasMatematicas.sh
+++ run/TablasMatematicas.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 rm TablasMatematicas.jar
-wget -O TablasMatematicas.jar https://github.com/logongas/TablasMatematicas/blob/master/dist/TablasMatematicas.jar?raw=true
+wget --no-cache -O TablasMatematicas.jar https://github.com/logongas/TablasMatematicas/blob/master/dist/TablasMatematicas.jar?raw=true
 java -jar "TablasMatematicas.jar" &
