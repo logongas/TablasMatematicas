60a78f326bbfe6b06ef64e1e782a7fe1aba77eb4
Lorenzo <lorenzo.profesor@gmail.com>
2022-02-15 17:24:52 +0100

    Versión inicial

---
+++ src/multiplicacion/Prueba.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package multiplicacion;
+
+/**
+ *
+ * @author logongas
+ */
+public class Prueba {
+    int a;
+    int b;
+    int aciertos;
+
+    public Prueba(int a, int b, int aciertos) {
+        this.a = a;
+        this.b = b;
+        this.aciertos = aciertos;
+    }
+    
+    
+}
